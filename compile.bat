41da0fce717033e91a2bbc4a2f9fc478fd3d0697
zaonirinku <zaonirinku@gmail.com>
2020-05-22 09:17:46 +0200

    minor edit

--- compile.bat
+++
@@ -1,4 +0,0 @@
-D:\VulkanSDK\1.2.135.0\Bin\glslangValidator.exe -V shaders/shader.vert -o shaders/vert.spv
-D:\VulkanSDK\1.2.135.0\Bin\glslangValidator.exe -V shaders/shader.frag -o shaders/frag.spv
-D:\VulkanSDK\1.2.135.0\Bin\glslangValidator.exe -V shaders/shadow.vert -o shaders/shadowvert.spv
-pause
