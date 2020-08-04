fe36e2878648d6b643b8d6ba670bd7fc30b3b1f5
zaonirinku <zaonirinku@gmail.com>
2020-08-04 15:37:47 +0200

    spotlights

---
+++ src/SpotLight.cpp
@@ -0,0 +1,74 @@
+#include "SpotLight.h"
+
+SpotLight::SpotLight(float px, float py, float pz, float dx, float dy, float dz, float r, float g, float b, float co) {
+	position = glm::vec3(px, py, pz);
+	direction = glm::vec3(dx, dy, dz);
+	color = glm::vec3(r, g, b);
+	cutoff = glm::cos(glm::radians(co));
+	torchlight = false;
+}
+
+float SpotLight::getPositionX() {
+	return position.x;
+}
+
+float SpotLight::getPositionY() {
+	return position.y;
+}
+
+float SpotLight::getPositionZ() {
+	return position.z;
+}
+
+void SpotLight::setPosition(float x, float y, float z) {
+	position = glm::vec3(x, y, z);
+}
+
+float SpotLight::getDirectionX() {
+	return direction.x;
+}
+
+float SpotLight::getDirectionY() {
+	return direction.y;
+}
+
+float SpotLight::getDirectionZ() {
+	return direction.z;
+}
+
+void SpotLight::setDirection(float x, float y, float z) {
+	direction = glm::vec3(x, y, z);
+}
+
+float SpotLight::getColorR() {
+	return color.x;
+}
+
+float SpotLight::getColorG() {
+	return color.y;
+}
+
+float SpotLight::getColorB() {
+	return color.z;
+}
+
+void SpotLight::setColor(float r, float g, float b) {
+	color = glm::vec3(r, g, b);
+}
+
+float SpotLight::getCutoff() {
+	return cutoff;
+}
+
+void SpotLight::setCutoff(float newCutoff) {
+	cutoff = glm::cos(glm::radians(newCutoff));
+}
+
+bool SpotLight::isTorchlight() {
+	return torchlight;
+}
+
+void SpotLight::actAsTorchlight(bool torchlightVal) {
+	torchlight = torchlightVal;
+	direction = -direction;
+}
---
+++ src/SpotLight.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+class SpotLight {
+public:
+	SpotLight(float px, float py, float pz, float dx, float dy, float dz, float r, float g, float b, float co);
+	float getPositionX();
+	float getPositionY();
+	float getPositionZ();
+	void setPosition(float x, float y, float z);
+	float getDirectionX();
+	float getDirectionY();
+	float getDirectionZ();
+	void setDirection(float x, float y, float z);
+	float getColorR();
+	float getColorG();
+	float getColorB();
+	void setColor(float r, float g, float b);
+	float getCutoff();
+	void setCutoff(float newCutoff);
+	bool isTorchlight();
+	void actAsTorchlight(bool torchlightVal);
+
+	void(*frameEvent)(SpotLight* spotLight, GLFWwindow* window, double deltaTime);
+private:
+	glm::vec3 position;
+	glm::vec3 direction;
+	glm::vec3 color;
+	float cutoff;
+
+	bool torchlight;
+};
+
